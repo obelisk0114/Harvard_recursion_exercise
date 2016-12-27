cb748e6ca9c3073778daa3518a24d33dc452cc28
obelisk0114 <obelisk0114@yahoo.com.tw>
2016-12-27 02:16:45 -0600

    Add characterCount
    Counting Occurrences of a Character in a String

---
+++ src/characterCount/Count.java
@@ -0,0 +1,32 @@
+package characterCount;
+
+import java.util.Scanner;
+
+public class Count {
+	public static int count(char ch, String str, int num) {
+		int tmp;
+		if (str == null || str.equals("")) {
+			return num;
+		}
+		if (str.charAt(0) == ch) {
+			tmp = count(ch, str.substring(1), ++num);
+		}
+		else {			
+			tmp = count(ch, str.substring(1), num);
+		}
+		return tmp;
+	}
+	
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Input the string : ");
+		String str = keyboard.next();
+		System.out.println("Input the character : ");
+		char ch = keyboard.next().charAt(0);
+		int ans = count(ch, str, 0);
+		System.out.println(ans);
+		
+		keyboard.close();
+	}
+
+}
